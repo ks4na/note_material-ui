6a779b4c86ca27bf8383d0e6a23e750d871496eb
ks4na <1017086648@qq.com>
2020-03-28 08:25:52 +0800

    添加 material-ui 依赖

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,36 @@ module.exports = {
             },
           },
         ],
+        exclude: [/node_modules/],
+      },
+      // 不对 node_modules 下的 css 文件进行模块化
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: [/node_modules/],
+      },
+      // imgs
+      {
+        test: /\.(jpg|jpeg|gif|bmp|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 5120, // 5k
+            name: '[name]_[hash:4].[ext]',
+          },
+        },
+        exclude: /assets[\\/]fonts/,
+      },
+      // fonts
+      {
+        test: /\.(ttf|svg|eot|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash:4].[ext]',
+          },
+        },
+        exclude: /assets[\\/]imgs/,
       },
     ],
   },
