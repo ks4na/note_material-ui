3ce4dbaa9f87c2e16d85a826cde0fd6830a4ba49
ks4na <1017086648@qq.com>
2020-04-02 08:49:42 +0800

    删除仓库中原有的jest笔记

--- test/examples/timerMocks/timerGame.ts
+++
@@ -1,7 +0,0 @@
-export default function timerGame(callback?: () => void): void {
-  console.log('Ready...go!')
-  setTimeout(() => {
-    console.log("Time's up -- stop!")
-    callback && callback()
-  }, 1000)
-}
--- test/examples/usingMatchers/strings.test.ts
+++
@@ -1,9 +0,0 @@
-test('there is no I in team', () => {
-  expect('team').not.toMatch(/I/)
-})
-
-test('there is a "stop" in christoph', () => {
-  expect('christoph').toMatch(/stop/)
-})
-
-export {}
